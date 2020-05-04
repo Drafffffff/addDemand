c5d6bd7995031bfa7fd6a3d89c3a3d7ab255d34e
drafffffff <i0.0ia@qq.com>
2020-05-04 21:23:39 +0800

    asfd

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from "vue";
+import App from "./App.vue";
+import { Button, Form, Field } from "vant";
+import { Cell, CellGroup } from "vant";
+import { NavBar } from "vant";
+import { Picker } from "vant";
+import { Popup } from "vant";
+import axios from "axios";
+Vue.prototype.$axios = axios; //全局注册，使用方法为:this.$axios
+Vue.use(Popup);
+Vue.use(Picker);
+Vue.use(NavBar);
+Vue.use(Cell);
+Vue.use(CellGroup);
+Vue.use(Field)
+  .use(Button)
+  .use(Form);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount("#app");
